4826e627bebece6c6e666431c4d990f7c14124bc
emamaia <ema_maia@hotmail.com>
2022-07-12 10:30:59 -0300

    subindo arquivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'exercicios2-angular';
+  
+ 
+  
+  constructor(){
+    
+  }
+  
 }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+home = 'Aqui vai o corpo da aplicação';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
